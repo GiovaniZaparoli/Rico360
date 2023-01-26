be1e2fbccba11ebc741576c94664b2109ea2dee3
giovani.zaparoli <giovani.zaparoli@leavening.com>
2023-01-26 15:55:27 -0300

    Change interface methods

--- app/Repositories/Contracts/CallRepositoryInterface.php
+++ app/Repositories/Contracts/CallRepositoryInterface.php
@@ -5,7 +5,7 @@ namespace App\Repositories\Contracts;
 interface CallRepositoryInterface
 {
     public function getAllCalls();
-    public function getCallBySid($sid);
-    public function createCall(array $call);
-    public function updateCall(string $sid, array $call);
+    public function getCallBySid(string $sid);
+    public function createCall(array $params);
+    public function updateCall(object $call, array $params);
 }
